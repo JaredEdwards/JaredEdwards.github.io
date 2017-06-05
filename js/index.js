b3fc21edb68a71b45e3070a048e327401543b373
jared edwards <jredwards519@gmail.com>
2017-06-05 11:17:42 -0400

    added work information to display

--- js/index.js
+++ js/index.js
@@ -36,6 +36,46 @@ const bio = {
   ]
 };
 
+const work = {
+  jobs: [
+    {
+      employer: 'Bodhi Works',
+      title: 'Front End Developer / Dev Ops',
+      location: 'Falls Church, VA',
+      dates: 'April 2017 - Present',
+      description: 'Started Agile workflow using one week sprints for product development towards a MVP for a Proximity Based Ordering System (PBOS) on an 8- week project. I was included in user stories, product design, and new feature implementation. The technologies used were Node.js, Express.js, JQuery, WebSockets, AWS EC2 including a load balancer, Route 53 and SSL, Docker, Twitter API integration, Redis.js, React Native, and Git for version control. Selected 3rd party packages best fit for feature implementation in the native application for both android and iOS. I experienced a system architecture for a scalable application that used Docker containers for each of its processes to interact in production as well as maintained consistent development environments.'
+    },
+    {
+      employer: 'Port City Brewing Company',
+      title: ' Beer Guide & Brewery Representative',
+      location: 'Alexandria, VA',
+      dates: 'June 2015 - Present',
+      description: 'Work in the tasting room and represent the 2015 Great American Beer Festival voted Best Small Craft Brewery in America delivering world class customer service, educating how Port City takes beer from grain to your glass.'
+    },
+    {
+      employer: 'Neighborhood Restaurant Group',
+      title: 'Bartender',
+      location: 'Merrifield, VA',
+      dates: 'January 2011 - June 2015',
+      description: 'Worked as bartender of good beer and food.'
+    },
+    {
+      employer: 'Aqueos',
+      title: 'Commercial Diver / Tender',
+      location: 'Lafayette, LA',
+      dates: 'April 2009 - May 2011',
+      description: 'Managed operations on deck to maintain and support any divers in water or in decompression. Responsible for full inventory of tools, gasses, and life support equipment.'
+    },
+    {
+      employer: 'Hometown',
+      title: 'Child',
+      location: 'Port Orchard, WA',
+      dates: 'May 1989 - March 2009',
+      description: 'Lived'
+    }
+  ]
+};
+
 /*
 * Structure for appending information
 */
