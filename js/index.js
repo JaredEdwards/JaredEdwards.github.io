23678291ffbf9aee601b7e25d6b17d77a9550124
jared edwards <jredwards519@gmail.com>
2017-06-05 10:57:11 -0400

    added header sctructure and information to page

--- js/index.js
+++ js/index.js
@@ -36,15 +36,40 @@ const bio = {
   ]
 };
 
-const headerName = `<h1 id='headerName'>name</h1><br />`;
-const headerRole = `<span id='headerRole'>role</span><hr>`;
+/*
+* Structure for appending information
+*/
+const headerName = `<h1 id='headerName'>myName</h1><br />`;
+const headerRole = `<span id='headerRole'>myRole</span><hr>`;
+const contactMobile =
+  '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">myMobile</span></li>';
+const contactEmail =
+  '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">myEmail</span></li>';
+const contactTwitter =
+  '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">myTwitter</span></li>';
+const contactGithub =
+  '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">myGithub</span></li>';
+const contactLocation =
+  '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">myLocation</span></li>';
 
 /*
 * Initial JQuery functions to append info.
 */
 
-// header
-const name = headerName.replace('name', bio.name);
-const role = headerRole.replace('role', bio.role);
+// header section
+
+const name = headerName.replace('myName', bio.name);
+const role = headerRole.replace('myRole', bio.role);
+const mobile = contactMobile.replace('myMobile', bio.contacts.mobile);
+const email = contactEmail.replace('myEmail', bio.contacts.email);
+const twitter = contactTwitter.replace('myTwitter', bio.contacts.twitter);
+const bitbucket = contactGithub.replace('myGithub', bio.contacts.github);
+const myLocation = contactLocation.replace('myLocation', bio.contacts.location);
+
 $('#header').prepend(role);
 $('#header').prepend(name);
+$('#topContacts').append(mobile);
+$('#topContacts').append(email);
+$('#topContacts').append(twitter);
+$('#topContacts').append(bitbucket);
+$('#topContacts').append(myLocation);
