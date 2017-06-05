ac7b6cbb8dbac610070b5b3e753199e8ad5eebc2
jared edwards <jredwards519@gmail.com>
2017-06-05 14:36:48 -0400

    added url for bodhi works for test

--- js/index.js
+++ js/index.js
@@ -43,6 +43,7 @@ const work = {
       title: 'Front End Developer / Dev Ops',
       location: 'Falls Church, VA',
       dates: 'April 2017 - Present',
+      url: 'www.bodhiworks.io',
       description: 'Started Agile workflow using one week sprints for product development towards a MVP for a Proximity Based Ordering System (PBOS) on an 8- week project. I was included in user stories, product design, and new feature implementation. The technologies used were Node.js, Express.js, JQuery, WebSockets, AWS EC2 including a load balancer, Route 53 and SSL, Docker, Twitter API integration, Redis.js, React Native, and Git for version control. Selected 3rd party packages best fit for feature implementation in the native application for both android and iOS. I experienced a system architecture for a scalable application that used Docker containers for each of its processes to interact in production as well as maintained consistent development environments.'
     },
     {
@@ -277,7 +278,8 @@ function displayWork(jobs) {
     //concatenate employer and title and append
     const employer = workEmployer.replace('employerName', job.employer);
     const title = workTitle.replace('jobTitle', job.title);
-    const employerTitle = employer + title;
+    let employerTitle = employer + title;
+    employerTitle = employerTitle.replace('#', job.url);
 
     //append each new iteration to the :last child
     $('.work-entry:last').append(employerTitle);
